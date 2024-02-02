3a6532f38437e796e556b59626608aca445a4f8d
mesoud5 <mesoudtemam5@gmail.com>
2024-02-02 13:05:12 -0700

    ohh

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,5 +1,3 @@
-// Define the 'add' function
-const add = (a, b) => a + b;
-
-// Export the 'add' function for use in other files
-// module.exports = { add };
+function add(a, b) {
+    return a + b;
+}
