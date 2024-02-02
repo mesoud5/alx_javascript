7dffbe0dbbdd60f98bcc976e662fdee834904ce8
mesoud5 <mesoudtemam5@gmail.com>
2024-02-02 12:57:34 -0700

    task 5

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,3 +1,5 @@
-const add = function (a, b) {
-    return a + b;
-};
+// Define a constant function named 'add'
+const add = (a, b) => a + b;
+
+// Now, you can use the 'add' function from anywhere
+console.log(add(3, 5)); // Output: 8
