c6d49ba7d8d7333b2db888fea14f0e2d4af6e04b
mesoud5 <mesoudtemam5@gmail.com>
2024-02-05 12:02:25 -0700

    task7

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 
 function addMeMaybe(number, theFunction) {
+  // Base case: Stop recursion when the number exceeds a certain limit
+  if (number > 10) {
+    return;
+  }
+
   // Increment the number
   number++;
 
@@ -10,6 +15,7 @@ function addMeMaybe(number, theFunction) {
   // Call the provided function with the incremented number
   theFunction(number);
 }
+
 
 module.exports = {
   addMeMaybe: addMeMaybe
