a12307b9f9fae795132463fd289ceed8765b5847
mesoud5 <mesoudtemam5@gmail.com>
2024-02-05 12:38:12 -0700

    task 7

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
 
 const addMeMaybe = (number, theFunction) => {
-    // Increment the number
     number++;
-  
-    // Recursive call
-    addMeMaybe(number, theFunction);
-  
-    // Call the provided function with the incremented number
-    theFunction(number);
-  }
-  
-  
-  module.exports.addMeMaybe = addMeMaybe;
-  
+    theFunction(number) ;
+};
+
+module.exports.addMeMaybe = addMeMaybe;
