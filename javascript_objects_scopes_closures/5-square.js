e64643f885c5b888145f1bd4778f7157a35e3edf
mesoud5 <mesoudtemam5@gmail.com>
2024-02-07 14:13:21 -0700

    task 55

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -3,7 +3,8 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle{
     constructor(size) {
-        super();
-        this.size = size;
+        super(size, size);
     }
 }
+
+module.exports = Square;
