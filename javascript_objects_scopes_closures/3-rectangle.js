614b5ed5704c7d4fb82a713cdce5fafda9c061dd
mesoud5 <mesoudtemam5@gmail.com>
2024-02-07 13:23:23 -0700

    task 3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -9,7 +9,7 @@ class Rectangle {
         for (let i = 0; i < this.height; i++) {
             let s = '';
             for (let j = 0; j < this.width; j++) {
-                s += 'x';
+                s += 'X';
             }
             console.log(s)
     }
