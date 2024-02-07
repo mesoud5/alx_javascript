46151e4517fef4ef16ddb8c3e2f948e381be16d1
mesoud5 <mesoudtemam5@gmail.com>
2024-02-07 14:29:46 -0700

    task 6

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+const Square = require('./5-square');
+
+
+class Square extends Square{
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+
+    }
+    
+    charPrint(c){
+        c = c || 'X' ;
+        for (let i = 0; i < this.height; i++) {
+            let s = '';
+            for (let j = 0; j < this.width; j++) {
+                s += c ;
+            }
+            console.log(s)
+
+            }
+
+    }
+}
+
+module.exports = Square;
