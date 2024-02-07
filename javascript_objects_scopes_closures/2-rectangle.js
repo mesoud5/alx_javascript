4bc12454e0ffe5568e24ee0e0a81a153adc5d7ab
mesoud5 <mesoudtemam5@gmail.com>
2024-02-07 10:58:49 -0700

    task 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,15 @@
 class Rectangle {
     constructor(w, h) {
         if (w <= 0 || h <= 0) {
+            
+            return{};
 
-        } else{
-            this.width = w;
-            this.height = h;
+        } 
+        this.width = w;
+        this.height = h;
         }
 
     }
-}
+
 
 module.exports = Rectangle;
