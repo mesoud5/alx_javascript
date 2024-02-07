0ffefc320259a37e936c861981ddc3d1a8f3632f
mesoud5 <mesoudtemam5@gmail.com>
2024-02-07 12:47:17 -0700

    task 22

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,20 +1,16 @@
 class Rectangle {
-    
     constructor(w, h) {
-        if (w <= 0 || h <= 0) {
-            // If w or h is 0 or not a positive integer, create an empty object
-        this.width = {};
-        this.height = {};
-    }
+        if ( w <= 0 || h <= 0 ) {
+
+        }
         else {
             this.width = w;
             this.height = h;
         }
+        
+
 
     }
-
-
-    
 }
 
 module.exports = Rectangle;
