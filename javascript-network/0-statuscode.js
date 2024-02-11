871eabbc8b0d0cbc4eda93936ef3eba30d091e88
mesoud5 <mesoudtemam5@gmail.com>
2024-02-11 12:53:29 -0700

    TASK 0

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,8 +1,8 @@
 //const request = require('request')
 
-//const url = "https://swapi.dev/api/people/1"
+//const URL = "https://swapi.dev/api/people/1"
 
-request(url, function(error, response, body){
+request(URL, function(error, response, body){
     if (error) {
         console.error(error);
     } else {
