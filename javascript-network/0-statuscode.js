570532b2c4ff1910acbf6a32d9d2e62ef0154985
mesoud5 <mesoudtemam5@gmail.com>
2024-02-11 12:39:06 -0700

    task 0

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -6,6 +6,6 @@ request(url, function(error, response, body){
     if (error) {
         console.error(error);
     } else {
-        console.log("code:" +response.statusCode);
+        console.log("code:" +' '+response.statusCode);
     }
 });
